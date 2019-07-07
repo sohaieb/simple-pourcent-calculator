77c10ecfbdb8b79d49cf705258a0adb4da668481
sohaieb <sohaiebsniper@gmail.com>
2019-07-07 17:25:57 +0100

    JS Pourcent Calculator v1.0

---
+++ scripts.js
@@ -0,0 +1,29 @@
+/** Variables init **/
+var resultatOutput = document.getElementById("resultat"),
+    btn = document.getElementById("calculateBTN"),
+    numberA = document.getElementById("a"),
+    numberB = document.getElementById("b");
+
+/** Functions def **/
+function calculer(e){
+    e.preventDefault();
+    var a = parseFloat(numberA.value),
+        b = parseFloat(numberB.value),
+        res = ((a/b)*100).toFixed(2);
+    if(b!==0){
+        resultatOutput.innerText = res + "%";
+    }else{
+        resultatOutput.classList.add("error");
+        resultatOutput.innerText = "Dévision par zero est impossible";
+    }
+}
+function resetElements(){
+    if(resultatOutput.classList.contains("error")){
+        resultatOutput.classList.remove("error");
+        resultatOutput.innerText = 0 + "%";
+    }
+}
+
+/** functions implementation **/
+numberB.addEventListener('keyup',resetElements);
+btn.addEventListener('click',calculer);
